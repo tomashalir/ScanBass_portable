6ed6453995b29e448e4c82d1ecc3adb9734475ba
tomashalir <tomas.halir@post.cz>
2025-11-01 17:19:08 +0100

    push push

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:8000";
+const API_BASE = "https://scanbass-backend.onrender.com";
 
 const form = document.getElementById("uploadForm");
 const dropzone = document.getElementById("dropzone");
